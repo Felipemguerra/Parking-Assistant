3ecd81b9aaa6e17ed9afb0d0f41faf3375215186
rehluct <scenteno96@gmail.com>
2018-03-21 21:46:16 -0400

    Map no location

--- app/src/main/java/edu/fsu/cs/mobile/parkingassistant/FindLocationFragment.java
+++ app/src/main/java/edu/fsu/cs/mobile/parkingassistant/FindLocationFragment.java
@@ -2,24 +2,34 @@ package edu.fsu.cs.mobile.parkingassistant;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class FindLocationFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
 
+public class FindLocationFragment extends Fragment {
+    protected MapView mMapView;
     private Button backBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_find_location, container, false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
         getViews(v);
         setText();
         setListeners();
+
         return v;
     }
+
 
     private void getViews(View v) {
         backBtn = v.findViewById(R.id.findBackBtn);
@@ -36,5 +46,48 @@ public class FindLocationFragment extends Fragment {
                 ((MainActivity)getActivity()).setMainFrag();
             }
         });
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.onResume();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        if (mMapView != null) {
+            mMapView.onPause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mMapView != null) {
+            try {
+                mMapView.onDestroy();
+            } catch (NullPointerException e) {
+                Log.e("destroy", "Error while attempting MapView.onDestroy(), ignoring exception", e);
+            }
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mMapView != null) {
+            mMapView.onLowMemory();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mMapView != null) {
+            mMapView.onSaveInstanceState(outState);
+        }
     }
 }
